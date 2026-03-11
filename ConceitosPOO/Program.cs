4bb64d7dd3ae94735923c3a6569d04a446dc4596
rosenclever <rosenclever@gmail.com>
2026-03-10 21:47:34 -0300

    falta implementar os metodos saque e deposito

--- ConceitosPOO/Program.cs
+++ ConceitosPOO/Program.cs
@@ -8,7 +8,7 @@ string nome = Console.ReadLine();
 Console.WriteLine("Informe o CPF do cliente:");
 string cpf = Console.ReadLine();
 cliente = new Cliente(nome, cpf);
-cliente.Nome = "";
+//cliente.Nome = "";
 Console.WriteLine($"Nome do cliente: {cliente.Nome}");
 Agencia agencia = new Agencia(123);
 agencia.Nome = "UGBVR";
@@ -18,11 +18,19 @@ Console.WriteLine("Numero: " + agencia.Numero);
 Console.WriteLine("Nome: " + agencia.Nome);
 Console.WriteLine("Telefone: " + agencia.Telefone);
 Console.WriteLine("Informe o número da conta:");
-long numero;
+long numero = Convert.ToInt64(Console.ReadLine());
 Console.WriteLine($"Informe o saldo inicial da conta {numero}:");
-decimal saldoInicial;
+decimal saldoInicial = decimal.Parse(Console.ReadLine());
 //imprimir os dados da conta
+Conta conta = new Conta(numero, saldoInicial);
+Console.WriteLine($"A conta número {conta.Numero} possui saldo {conta.Saldo}");
 //depositar 1000 na conta
+conta.Deposito(1000);
 //exibir saldo
+Console.WriteLine($"A conta número {conta.Numero} possui saldo {conta.Saldo}");
 //sacar 500 da conta
+conta.Saque(500);
 //exibir saldo
+Console.WriteLine($"A conta número {conta.Numero} possui saldo {conta.Saldo}");
+
+//A partir do vídeo identifique as tecnologias utilizadas para o desenvolvimento das soluções apresentadas. Recomenda-se ler os artigos do blog relacionados ao vídeo.
